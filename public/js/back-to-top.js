d42aa33da472b8850ab5095455f1c7f0dd406a94
Yuan Meng <hail_to_psyche@berkeley.edu>
2024-11-22 20:23:47 -0800

    add scroll up button

---
+++ public/js/back-to-top.js
@@ -0,0 +1,19 @@
+// Back to Top Button Functionality
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopButton = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTopButton.style.display = "block";
+    } else {
+      backToTopButton.style.display = "none";
+    }
+  });
+
+  backToTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
